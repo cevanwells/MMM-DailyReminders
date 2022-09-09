a93cb0acc8243247a6b91ee980e40e11f4d6d56f
Chris Wells <shiny.job6867@fastmail.com>
2022-09-09 09:29:14 -0400

    Update MMM-DailyReminders.js

--- MMM-DailyReminders.js
+++ MMM-DailyReminders.js
@@ -1,1 +1,21 @@
+/* Magic Mirror
+ * Module: MMM-DailyReminders
+ *
+ * By Chris Wells
+ * Apache-2 Licensed
+ */
+
+Module.register("MMM-DailyReminders", {
+  // Default module config.
+  defaults: {
+    text: "Hello World!",
+  },
+
+  // Override dom generator.
+  getDom: function () {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = this.config.text;
+    return wrapper;
+  },
+});
 
